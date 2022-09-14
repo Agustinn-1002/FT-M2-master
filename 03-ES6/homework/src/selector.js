58f443c7d5124f811faf94a3eb4ccb4dd3fdaf9d
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2022-09-14 20:55:54 -0300

    demo

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -18,7 +18,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if (selector[0] === '#') return 'id';
+  else if (selector[0] === '.') return 'class';
+  else if (selector.includes('.')) return "tag.class";
+  else return 'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +33,39 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = function (e) {
+      let aux = selector.slice(1);
+      let idAux = e.id
+      return aux == idAux;
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = function (e) {
+      let aux = selector.slice(1);
+      let classAux = e.className
+      let clases = classAux.split(' ')
+      for(let key in clases){
+        if (clases[key] === aux)return true;
+      }
+      return false
+    }
+
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function (e) {
+      let elements = selector.split('.')
+      let newElement = e.className.split(' ')
+      if(elements[0] === e.tagName.toLowerCase()){
+        for(let values in newElement){
+          if(newElement[values] === elements[1]){
+            return true
+          }
+        }
+      }
+      return false
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function (e) {
+      return e.tagName.toLowerCase() === selector.toLowerCase()
+    }
   }
   return matchFunction;
 };
