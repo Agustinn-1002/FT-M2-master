082ad2fdfd938a650470dfab84fd5a5773115315
Generico Infoandina <agupasten@gmail.com>
2022-09-22 17:45:09 -0300

    Complete

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import '../styles/styleCard.css';
 
-export default function Card(props) {
+export default function Card({name, img , max , min , onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className='contain'>
+      <button className='boton' onClick={() => onClose()}>X</button>
+      <p>{name}</p>
+      <div className='caracteristicas'>
+        <div className='temperaturas'>
+          <ul>
+            <li>Min</li>
+            <li>{min}</li>
+          </ul>
+          <ul>
+            <li>Max</li>
+            <li>{max}</li>
+          </ul>
+        </div>
+        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="" />
+      </div>
+    </div>
+  )
 };
