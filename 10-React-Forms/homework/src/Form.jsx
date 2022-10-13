fa88a80c35629533e255159cb824df64d447eacd
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2022-10-12 23:50:59 -0300

    Complete

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function  Form() {
+
+export default function Form(props) {
+  /* const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('') */
+
+  const [input , setInput] = useState({
+    username: '',
+    password: ''
+  })
+  const [errors, setErrors] = React.useState({});
+
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  function validate(input) {
+    let errors = {};
+    if (!input.username) {
+      errors.username = 'Username is required';
+    } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+      errors.username = 'Username is invalid';
+    } else if (!/(?=.*[0-9])/.test(input.password)) {
+      errors.password = 'Password is invalid';
+    } else if (!input.password){
+      errors.password = 'Password is required';
+    }
+    
+    return errors;
+  };
+
   return (
-      <div>
-        Componente Form
-      </div>
+      <form>
+        <div>
+          <label>Username:</label>
+          <input type="text" name='username' value={input.username} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name='password' value={input.password} onChange={handleInputChange}/>
+        </div>
+          <input type="submit" />
+      </form>
   )
 }
